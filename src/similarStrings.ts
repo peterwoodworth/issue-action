756d85813b40884c08d6265914f6d30494d562e5
Noah Litov <noahlitov@gmail.com>
2021-04-04 20:32:53 -0700

    fixed comment description and added round-up

--- src/similarStrings.ts
+++ src/similarStrings.ts
@@ -7,10 +7,11 @@ export const similarStrings = (str1: string, str2: string, similarity: number = 
     // Regex for removing punctuation and replacing with empty string
     str1 = str1.replace(/ |_|-|\(|\)|:|\[|\]|	|\./gi, '');
     str2 = str2.replace(/ |_|-|\(|\)|:|\[|\]|	|\./gi, '');
-  ​
-    // levenshtein returns a value between 0 and 1 based on how similar the strings are. Higher = more similar
-    // based on number of changes required to get from one string to the other
-    if(levenshtein(str1, str2) <= ((str1.length + str2.length) / 2) * similarity)
+  
+    // levenshtein returns a value between 0 and the length of the strings being compared. This
+    // represents the number of character differences between compared strings. We compare this
+    // with a set percentage of the average length of said strings
+    if(levenshtein(str1, str2) <= Math.ceil(((str1.length + str2.length) / 2) * similarity))
       return true;
     else
       return false;
